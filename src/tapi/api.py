5399dc7e47ffdc335ff674161c4ef50c1c3ad242
ritonun <73615644+ritonun@users.noreply.github.com>
2025-10-04 11:29:27 +0200

    get stops_schedules response

--- src/tapi/api.py
+++ src/tapi/api.py
@@ -1,12 +1,14 @@
 import requests
 
+
 class TisseoAPI:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, timeout_request=10):
         """
         Initialisation du client API
         :param api_key: Votre cle api Tisseo
         """
         self.api_key = api_key
+        self.timeout_request = timeout_request
         # https://api.tisseo.fr/v2/<nom du service>.<format>?<paramètres>&key=<votre_clé>
         self.base_url = "https://api.tisseo.fr/v2/"
 
@@ -27,14 +29,39 @@ class TisseoAPI:
         full_url = f"{self.base_url}{service}.{format}?{query_url[1:]}"
         return full_url
 
-    def get_stops_schedule(self):
+    def _request(self, service: str, format: str = "json", parametres: dict = {}):
+        url = self._build_url(service, format=format, parametres=parametres)
+
+        debug_msg = f"service: {service}, format: {format}, parametres: {parametres}, url: {url}"
+
+        try:
+            response = requests.get(url, timeout=self.timeout_request)
+            response.raise_for_status()
+            if format.lower() == "json":
+                return response.json()
+            else:
+                return response.text
+        except requests.exceptions.Timeout:
+            raise RuntimeError(f"Tisséo API request a dépassé le timeout {self.timeout_request}; {debug_msg}")
+        except requests.exceptions.HTTPError as e:
+            raise RuntimeError(f"HTTP Error en reponse de la requete Tisseo API {debug_msg}: {e}")
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(f"Fail de l'appel Tisseo API {debug_msg}: {e}")
+        except ValueError:
+            raise RuntimeError(f"Erreur lors du parsing du json en reponse de l'api {debug_msg}")
+
+    def get_stops_schedule(self, parametres: dict = {}):
         """
         Faire une requete a l'api tisseo pour obtenir les prochains passages de cet arret
         cf. p26 api doc
         """
 
-        parametres = {
-            "number": 10,
+        parametres_api_default = {
             "displayRealTime": 1,
-
+            "maxDays": 1
         }
+
+        if not parametres:
+            parametres = parametres_api_default
+
+        return self._request("stops_schedules", format="json", parametres=parametres)
